239a5aea05c7cbc549312397929172fd274eb129
heitorlisboa <heitor01101000@gmail.com>
2022-10-15 19:02:05 -0300

    improvement: allow more props for `Checkbox`

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,18 +2,19 @@ import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 import clsx from 'clsx';
 
-export type CheckboxProps = {
+export type CheckboxProps = CheckboxPrimitive.CheckboxProps & {
   id: string;
   label: string;
   className?: string;
 };
 
-export function Checkbox({ id, label, className }: CheckboxProps) {
+export function Checkbox({ id, label, className, ...props }: CheckboxProps) {
   return (
     <div className={clsx('flex items-center gap-2', className)}>
       <CheckboxPrimitive.Root
         id={id}
         className="bg-gray-800 w-6 h-6 p-0.5 rounded"
+        {...props}
       >
         <CheckboxPrimitive.Indicator>
           <Check className="w-5 h-5 text-cyan-500" weight="bold" />
